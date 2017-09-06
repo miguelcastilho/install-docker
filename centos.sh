0e21b199a73a45ee07ed69fa541bb7580421cbeb
Miguel Castilho Dias <miguelcastilhodias@gmail.com>
2017-09-06 15:20:57 +0100

    Create centos.sh

---
+++ centos.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo -E yum install -y yum-utils device-mapper-persistent-data lvm2
+
+sudo -E yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+sudo -E yum makecache fast
+
+sudo -E yum install docker-ce -y
+
+sudo systemctl start docker
